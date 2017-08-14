68e095596a33dd87b0bdeeee51a33ab23b90cb4d
Sayapin Alexander <a.sayapin@a1-systems.com>
2017-08-14 16:01:42 +0300

    + Added interfaces.

---
+++ src/StreamUtils.php
@@ -0,0 +1,67 @@
+<?php
+
+class StreamUtils {
+    public function readUnsignedByte(InputStream $stream) {
+
+    }
+
+    public function putUnsignedByte(OutputStream $stream) {
+
+    }
+
+    public function readSignedByte(InputStream $stream) {
+
+    }
+
+    public function putSignedByte(OutputStream $stream) {
+
+    }
+
+    public function readUnsignedShort(InputStream $stream) {
+
+    }
+
+    public function putUnsignedShort(OutputStream $stream) {
+
+    }
+
+    public function readSignedShort(InputStream $stream) {
+
+    }
+
+    public function putSignedShort(OutputStream $stream) {
+
+    }
+
+    public function readUnsignedInt(InputStream $stream) {
+
+    }
+
+    public function putUnsignedInt(OutputStream $stream) {
+
+    }
+
+    public function readSignedInt(InputStream $stream) {
+
+    }
+
+    public function putSignedInt(OutputStream $stream) {
+
+    }
+
+    public function readUnsignedLong(InputStream $stream) {
+
+    }
+
+    public function putUnsignedLong(OutputStream $stream) {
+
+    }
+
+    public function readSignedLong(InputStream $stream) {
+
+    }
+
+    public function putSignedLong(OutputStream $stream) {
+
+    }
+}
